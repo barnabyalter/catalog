91f3142e9f45b92ec3dde16cb47ccc41a6486877
Adam Wead <amsterdamos@gmail.com>
2012-10-05 11:37:20 -0400

    Post merge fixes: catalog_controller

--- app/controllers/catalog_controller.rb
+++ app/controllers/catalog_controller.rb
@@ -5,7 +5,9 @@ class CatalogController < ApplicationController
 
   include Blacklight::Catalog
   include BlacklightHighlight::ControllerExtension
-  include Rockhall::EadSolrMethods
+  include Rockhall::ControllerBehaviors
+
+  before_filter :query_child_components, :only => :show
 
   configure_blacklight do |config|
     config.default_solr_params = {
@@ -160,6 +162,28 @@ class CatalogController < ApplicationController
     config.add_show_field 'pubnum_display',       :label => 'Publisher Number:'
     config.add_show_field 'id',                   :label => 'OCLC No.:'
 
+    # Fields specific to ead components
+    config.add_show_field 'accessrestrict_display',     :label => 'Access Restrictions:'
+    config.add_show_field 'accruals_display',           :label => 'Accruals:'
+    config.add_show_field 'acqinfo_display',            :label => 'Acquistions:'
+    config.add_show_field 'altformavail_display',       :label => 'Alt. Form:'
+    config.add_show_field 'appraisal_display',          :label => 'Appraisal:'
+    config.add_show_field 'arrangement_display',        :label => 'Arrangement:'
+    config.add_show_field 'custodhist_display',         :label => 'Custodial History:'
+    config.add_show_field 'fileplan_display',           :label => 'File Plan:'
+    config.add_show_field 'originalsloc_display',       :label => 'Originals:'
+    config.add_show_field 'phystech_display',           :label => 'Physical Tech:'
+    config.add_show_field 'processinfo_display',        :label => 'Processing:'
+    config.add_show_field 'relatedmaterial_display',    :label => 'Related Material:'
+    config.add_show_field 'separatedmaterial_display',  :label => 'Separated Material:'
+    config.add_show_field 'scopecontent_display',       :label => 'Scope and Content:'
+    config.add_show_field 'userestrict_display',        :label => 'Usage Restrictions:'
+    config.add_show_field 'physdesc_display',           :label => 'Physical Description:'
+    config.add_show_field 'dimensions_display',         :label => 'Dimensions:'
+    config.add_show_field 'langmaterial_display',       :label => 'Language:'
+    config.add_show_field 'note_display',               :label => 'Notes:'
+    config.add_show_field 'accession_display',          :label => 'Accession Numbers:'
+
     # "fielded" search configuration. Used by pulldown among other places.
     # For supported keys in hash, see rdoc for Blacklight::SearchFields
     #
@@ -240,28 +264,6 @@ class CatalogController < ApplicationController
     # If there are more than this many search results, no spelling ("did you
     # mean") suggestion is offered.
     config.spell_max = 5
-  end
-
-  def show
-    ead_id = get_field_from_solr("ead_id",params[:id])
-    if ead_id.nil?
-      super
-    else
-
-      @components = Hash.new
-      @components[:first] = get_component_docs_from_solr(ead_id,{ :level => "1"})
-      parent_ref_list = get_field_from_solr("parent_ref_list",params[:id])
-      unless parent_ref_list.nil?
-        parent_ref_list.each do |ref|
-          @components[ref.to_sym] = get_component_docs_from_solr(ead_id,{ :parent_ref => ref.to_s})
-        end
-      end
-
-      params[:solr_id] = params[:id]
-      params[:id] = ead_id
-      super
-
-    end
   end
 
 end
